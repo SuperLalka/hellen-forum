67c00c56322d3499bbe2bd7932c63e96afd1d1cf
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-14 20:16:15 +0300

    Creates models of forum objects in the DB
    Also migrations to them

--- sections/admin.py
+++ sections/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Sections)
+class SectionsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    search_fields = ['name']
+
+
+@admin.register(models.Subsections)
+class SubsectionsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'section']
+    search_fields = ['name']
+
+
+@admin.register(models.Topics)
+class TopicsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subsection']
+    search_fields = ['name']
+
+
+@admin.register(models.Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'topic']
--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=80, help_text="Enter category name")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Sections(models.Model):
+    name = models.CharField(max_length=50, help_text="Enter section name")
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE,
+                                 null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+
+class Subsections(models.Model):
+    name = models.CharField(max_length=50, help_text="Enter subsection name")
+    section = models.ForeignKey('Sections', on_delete=models.CASCADE,
+                                null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Подраздел'
+        verbose_name_plural = 'Подразделы'
+
+
+class Topics(models.Model):
+    name = models.CharField(max_length=50, help_text="Enter topic name")
+    subsection = models.ForeignKey('Subsections', on_delete=models.CASCADE,
+                                   null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+
+
+class Comments(models.Model):
+    text = models.TextField(max_length=300)
+    topic = models.ForeignKey('Topics', on_delete=models.CASCADE,
+                              null=True, blank=True)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
