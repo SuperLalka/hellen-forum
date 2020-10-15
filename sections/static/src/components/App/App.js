00fa35fd318e465beea98583c271cb076ad368f8
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-15 22:26:06 +0300

    Makes changes to other react-components

--- sections/static/src/components/App/App.js
+++ sections/static/src/components/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-import SubSections from '../SubSections/SubSections';
-import SectionsList from '../SectionsList/SectionsList';
-import Topics from '../Topics/Topics';
+import Authentication from '../Authentication/Authentication';
 import Comments from '../Comments/Comments';
+import Registration from '../Registration/Registration';
+import SectionsList from '../SectionsList/SectionsList';
+import SubSections from '../SubSections/SubSections';
+import Topics from '../Topics/Topics';
+
 import css from './App.css';
 
 import {
@@ -14,16 +17,30 @@ import {
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: null,
+        };
+    }
+
+    isAuthorized(token) {
+        this.setState({
+            token: token
+        });
+    }
 
     render() {
         return (
             <Router>
                 <div className="App">
+                    <Authentication isAuthorized={(token) => this.isAuthorized(token)} />
                     <Switch>
-                        <Route path="/section/subsection/topics/:topic_id" component={Comments} />
-                        <Route path="/section/subsection/:subsection_id" component={Topics} />
-                        <Route path="/section/:section_id" component={SubSections} />
-                        <Route path="/" component={SectionsList} />
+                        <Route path="/registration" component={Registration}/>
+                        <Route path="/section/subsection/topics/:topic_id" component={Comments}/>
+                        <Route path="/section/subsection/:subsection_id" component={Topics}/>
+                        <Route path="/section/:section_id" component={SubSections}/>
+                        <Route exact path="/" component={SectionsList}/>
                     </Switch>
                 </div>
             </Router>
