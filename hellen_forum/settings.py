d34bdb44f02e7e2abf885a7596812085ffe051d5
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-22 18:37:28 +0300

    Configures the path to media files in the project settings
    Also expands files requirements & gitignore

--- hellen_forum/settings.py
+++ hellen_forum/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'sections/static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'sections/media')
